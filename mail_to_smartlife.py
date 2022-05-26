86df4ca9c2663091020caedd5667d66bc460490e
guigonzalez <guidogermang@gmail.com>
2022-05-26 13:37:55 -0300

    Modifico los indices de la variable index

--- mail_to_smartlife.py
+++ mail_to_smartlife.py
@@ -76,7 +76,7 @@ def getRigsFromMail(list_mails):
     for body in list_mails:
         for m in re.finditer('.p', body):
             index = m.start()
-            string = body[index]+body[index+1]+body[index+2]+body[index+3]+body[index+4]
+            string = body[index+1]+body[index+2]+body[index+3]+body[index+4]+body[index+5]
             list_rigs.append(string)
             print('RIG CON ERROR: ',string)
     return list_rigs
